1e6e170ce9ef5d3798f917a3670433769059274e
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-12-11 15:57:01 -0500

    format adjustments

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -2,7 +2,6 @@ const { Thought, User } = require("../models");
 
 const thoughtController = {
 
-
     // get all thoughts 
     getAllThought(req, res) {
       Thought.find({})
@@ -85,6 +84,34 @@ const thoughtController = {
             res.json(dbUserData);
           })
           .catch(err => res.json(err));
+    },
+
+    // add reaction
+    addReaction({ params, body }, res) {
+      Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $push: { replies: body } },
+        { new: true }
+      )
+        .then(dbUserData => {
+          if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id!' });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    // remove reaction
+    removeReaction({ params }, res) {
+      Thought.findOneAndUpdate(
+        { _id: params.ThoughtId },
+        { $pull: { replies: { reactionId: params.reactionId } } },
+        { new: true }
+      )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
     }
 }
 
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -5,8 +5,8 @@ const {
     addThought,
     updateThought,
     removeThought, 
-    addReply, 
-    removeReply
+    addReaction, 
+    removeReaction
 } = require("../../controllers/thought-controller.js");
 
 // /api/thoughts
@@ -25,17 +25,18 @@ router
     .route("/:userId")
     .post(addThought);
 
-// /api/thoughts/<userId>/<thoughts>
+// /api/thoughts/<userId>/<thoughtId>
 router
     .route("/:userId/:thoughtId")
-    // .put(updateThought)
+    .put(addReaction)
     .delete(removeThought);
 
 // // /api/thoughts/<userId>/<thoughts>
 // router.route("/:userId/:thoughtsId").put(addReply).delete(removeThought); // USE THIS FOR LINE ABOVE -- .put
 
-// // /api/<userId>/<thoughtId>/<replyid> - deletion
-// router.route("/:userId/:thoughtId/:replyId").delete(removeReply);  // USE THIS FOR THE REPLY DELETION
+// /api/<userId>/<thoughtId>/<replyid> - deletion
+router.route("/:userId/:thoughtId/:reactionId")
+    .delete(removeReaction);
 
 
 module.exports = router;
